e017c26a2de758a28c48a7d409324f9364403c8c
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2024-02-21 00:11:14 -0500

    feat: :sparkles: JS content
    JS content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 import {} from "./module/facturacion.js"
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+
+//open|close menu functionality
+menuBtn.addEventListener("click", ()=> {
+    menu.classList.toggle("nav-toggle");
+})
